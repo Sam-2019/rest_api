18d88033eaf0397f4f965a13565f5b77e5bebebd
Sam-2019 <samtmartey@gmail.com>
2022-01-20 22:42:35 +0000

    added User, Institution routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'institution/index'
+  get 'institution/show'
+  get 'institution/update'
+  get 'institution/delete'
+  get 'user/index'
+  get 'user/show'
+  get 'user/update'
+  get 'user/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
