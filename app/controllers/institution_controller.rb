bfa62ad1bf90771d603d85dac9be654bf7b88ddd
Sam-2019 <samtmartey@gmail.com>
2022-01-21 23:17:08 +0000

    added json api

--- app/controllers/institution_controller.rb
+++ app/controllers/institution_controller.rb
@@ -1,15 +1,20 @@
 class InstitutionController < ApplicationController
   def index
-  institutions = Institution.all
+    institutions = Institution.all
+        render json: institutions.to_json
   end
 
   def show
-  institution = Institution.find params[:id]
+    institution = Institution.find params[:id]
+        render json: institution.to_json
   end
 
   def update
   end
 
   def delete
+  end
+
+  def cretae
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,20 @@
 class UserController < ApplicationController
   def index
     users = User.all
+        render json: users.to_json
   end
 
   def show
     user = User.find params[:id]
+        render json: user.to_json
   end
 
   def update
   end
 
   def delete
+  end
+
+  def cretae
   end
 end
