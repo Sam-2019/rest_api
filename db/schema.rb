de557a82cdb76f36b3b209c103fcccea0b84dd9f
Sam-2019 <samtmartey@gmail.com>
2022-01-22 10:56:13 +0000

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_220952) do
+ActiveRecord::Schema.define(version: 2022_01_22_104050) do
 
   create_table "institutions", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_01_20_220952) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
