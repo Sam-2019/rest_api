df2db6c2250202033a819ba0c31924258c21bc3b
Sam-2019 <samtmartey@gmail.com>
2022-01-22 11:53:10 +0000

    updated create actions HTTP verbs

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   get 'institution/show'
   get 'institution/update'
   get 'institution/delete'
+  post 'institution/create'
   get 'user/index'
   get 'user/show'
   get 'user/update'
   get 'user/delete'
+  post 'user/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
