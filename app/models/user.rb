b3197f87d31cf68f55954931f6b24f70e03b490d
Sam-2019 <samtmartey@gmail.com>
2022-01-22 10:56:35 +0000

    model validations and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     validates :first_name, presence: true, length: {maximum: 15}
     validates :last_name, presence: true, length: {maximum: 50}
-    validates :email, presence: true, length: {maximum: 250}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 250},
+                        format: {with: VALID_EMAIL_REGEX}, 
+                        uniqueness: {case_sensitive: false}
 end
