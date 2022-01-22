26eaa0ed62ee25456ee31012881a368dd04ecfec
Sam-2019 <samtmartey@gmail.com>
2022-01-22 20:13:54 +0000

    restored association

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,4 +1,5 @@
 class Institution < ApplicationRecord
+    has_many :users
     validates :name, presence: true, length: {maximum: 50 }
     validates :location, presence: true, length: {maximum: 50 }
 end
