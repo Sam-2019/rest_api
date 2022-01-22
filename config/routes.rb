0e66999afe18da5bf5a714fb9015957e8480123a
Sam-2019 <samtmartey@gmail.com>
2022-01-22 19:31:05 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get 'institution/index'
   get 'institution/show'
-  get 'institution/update'
-  get 'institution/delete'
+  patch 'institution/update'
+  delete 'institution/destroy'
   post 'institution/create'
   get 'user/index'
   get 'user/show'
-  get 'user/update'
-  get 'user/delete'
+  patch 'user/update'
+  delete 'user/destroy'
   post 'user/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
