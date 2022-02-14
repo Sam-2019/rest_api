151c9828e6193d883e9c7ace54d854cc59f72326
Sam-2019 <samtmartey@gmail.com>
2022-02-14 22:44:10 +0000

    added search_by_location, search_by_name

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,6 +2,9 @@ class Institution < ApplicationRecord
     has_many :users
     validates :name, presence: true, length: {maximum: 50 }
     validates :location, presence: true, length: {maximum: 50 }
+
+    scope :search_by_location, -> (location = nil) { where(location: location) }
+    scope :search_by_name, -> (name = nil) { where(name: name) }
 end
 
 # == Schema Information
