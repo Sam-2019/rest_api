4f3d3366eb9edcb5863950b4ee65801b040eaf87
Sam-2019 <samtmartey@gmail.com>
2022-02-17 22:54:13 +0000

    added assm

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,10 +1,31 @@
 class Institution < ApplicationRecord
+    include AASM
+
     has_many :users
     validates :name, presence: true, length: {maximum: 50 }
     validates :location, presence: true, length: {maximum: 50 }
 
     scope :search_by_location, -> (location = nil) { where(location: location) }
     scope :search_by_name, -> (name = nil) { where(name: name) }
+
+
+    aasm column: :state do # default column: aasm_state
+      state :not_verified, initial: true
+      state :verified, :not_approved, :approved
+  
+      event :verify do
+        transitions from: :not_verified, to: :verified
+      end
+  
+      event :pending_approval do
+        transitions from: :verified, to: :not_approved
+      end
+
+      event :approve do
+        transitions from: :not_approved, to: :approved
+      end
+    end
+  
 end
 
 # == Schema Information
