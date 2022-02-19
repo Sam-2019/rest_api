fafd1750f7bdd59a713971884a278346b20dbb71
Sam-2019 <samtmartey@gmail.com>
2022-02-19 19:33:10 +0000

    rm approved, verified columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_225934) do
+ActiveRecord::Schema.define(version: 2022_02_19_192402) do
 
   create_table "institutions", force: :cascade do |t|
     t.string "name"
