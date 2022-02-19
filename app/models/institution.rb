6a85ae340e10442aa51e2da89f48d8ce2e3837cc
Sam-2019 <samtmartey@gmail.com>
2022-02-19 20:34:19 +0000

    added model & aasm callbacks

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,8 +1,16 @@
 class Institution < ApplicationRecord
     include AASM
 
-    has_many :users
-    validates :name, presence: true, length: {maximum: 50 }
+    after_create :log_create_action
+    after_save :log_create_action
+
+    after_update :log_update_action
+    after_destroy :log_delete_action
+
+    has_many :users, dependent: :destroy
+    validates_associated :users
+
+    validates :name, presence: true, length: {minimum: 3 ,maximum: 50 }
     validates :location, presence: true, length: {maximum: 50 }
 
     scope :search_by_location, -> (location = nil) { where(location: location) }
@@ -12,6 +20,8 @@ class Institution < ApplicationRecord
     aasm column: :state do # default column: aasm_state
       state :not_verified, initial: true
       state :verified, :not_approved, :approved
+
+      after_all_transitions :log_status_change
   
       event :verify do
         transitions from: :not_verified, to: :verified
@@ -24,6 +34,24 @@ class Institution < ApplicationRecord
       event :approve do
         transitions from: :not_approved, to: :approved
       end
+    end
+
+    private
+
+    def log_create_action
+      puts 'Institution created'
+    end
+
+    def log_update_action
+      puts 'Institution updated'
+    end
+
+    def log_delete_action
+      puts 'Institution deleted'
+    end
+
+    def log_status_change
+      puts "changing from #{aasm.from_state} to #{aasm.to_state} (event: #{aasm.current_event})"
     end
   
 end
