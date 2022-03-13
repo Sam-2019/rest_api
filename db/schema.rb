c296d830bf1e46b0831ac6339415ec79f4db8588
Sam-2019 <samtmartey@gmail.com>
2022-03-13 22:31:53 +0000

    updated users schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_192402) do
+ActiveRecord::Schema.define(version: 2022_02_19_214237) do
 
   create_table "institutions", force: :cascade do |t|
     t.string "name"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 2022_02_19_192402) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "state"
+    t.integer "institutions_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["institutions_id"], name: "index_users_on_institutions_id"
   end
 
+  add_foreign_key "users", "institutions", column: "institutions_id"
 end
