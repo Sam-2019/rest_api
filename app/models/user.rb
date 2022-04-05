288568f708be550423597cf923ddb8c8c673675d
Sam-2019 <samtmartey@gmail.com>
2022-04-05 23:52:32 +0100

    email validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,7 @@ class User < ApplicationRecord
 
     validates :first_name, presence: true, length: {minimum: 3 ,maximum: 15}
     validates :last_name, presence: true, length: {minimum: 3 ,maximum: 50}
-    validates :email, presence: true, uniqueness: {case_sensitive: false}, 
-                      'valid_email_2/email': { mx: true, disposable: true }, 
+    validates :email, presence: true, uniqueness: {case_sensitive: false},'valid_email_2/email': { mx: true, disposable: true }
 
     scope :search_by_name, -> (query = nil) { where("first_name LIKE ? OR last_name LIKE ? ", "%" + query + "%", "%" + query + "%") }
 
