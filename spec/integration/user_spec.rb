97baf64aff587c11145832c9d6ac3976669b03ae
Samuel Martey <sam@encodevlabs.com>
2022-06-16 19:14:17 +0000

    updated integration files

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -1,9 +1,9 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/user', type: :request do
-    path "/api/v1/user" do
+    path "/user" do
         post "Create a User" do
-          tags "users"
+          tags "User"
           consumes "application/json"
           parameter name: :user, in: :body, schema: {
             type: :object,
@@ -12,7 +12,7 @@ RSpec.describe 'api/v1/user', type: :request do
               first_name: { type: :string },
               last_name: { type: :string },
               email: { type: :string },
-              institutions_id: { type: :integer }
+              institution_id: { type: :integer }
             },
             required: ['id', 'first_name','last_name', 'email' ],
           }
@@ -26,6 +26,39 @@ RSpec.describe 'api/v1/user', type: :request do
             run_test!
           end
 
+        end
+      end
+
+      path '/user/{id}' do
+        get 'Retrieve an user' do
+          tags 'User'
+          produces 'application/json'
+          parameter name: :id, in: :path, type: :string, schema: {
+            type: :object,
+            properties: {
+                id: { type: :integer },
+                first_name: { type: :string },
+                last_name: { type: :string },
+                email: { type: :string },
+                institution_id: {type: :integer}
+            },
+              required: ['id', 'first_name', 'last_name', 'email', 'institution_id' ],
+            }
+
+          response '200', 'user found' do
+            let(:id) { User.create(first_name: 'Major', last_name: 'Martey', email: 'major@gmail.com', institution_id: 26 ).id }
+            run_test!
+          end
+    
+          response '404', 'user not found' do
+            let(:id) { 'invalid' }
+            run_test!
+          end
+    
+          response '406', 'unsupported accept header' do
+            let(:'Accept') { 'application/foo' }
+            run_test!
+          end
         end
       end
 
