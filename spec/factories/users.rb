bdaaee1d1ddf640a8b1460fa4b803bf68dd7b6d4
Sam-2019 <samtmartey@gmail.com>
2022-09-21 22:26:30 +0100

    added model factory bots

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    sequence :first_name do |n|
+      "First +#{n}"
+    end
+    sequence :last_name do |n|
+      "Last+#{n}"
+    end
+    sequence :email do |n|
+      "email+#{n}@example.com"
+    end
+  end
+end
