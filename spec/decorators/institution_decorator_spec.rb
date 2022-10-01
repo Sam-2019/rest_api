ed090a7aea110e1b84dd7f58ef625dde807642c5
Sam-2019 <samtmartey@gmail.com>
2022-10-01 20:54:11 +0100

    added decorator test

--- spec/decorators/institution_decorator_spec.rb
+++ spec/decorators/institution_decorator_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe InstitutionDecorator do
+    let(:institution) { build(:institution) }
+
+    describe 'institution' do
+        it 'name' do
+          decorated_organization = ActiveDecorator::Decorator.instance.decorate(institution)
+      
+          expect(decorated_organization.institution_name).to eq('School+1')
+          expect(decorated_organization.institution_name).to include("School+")
+        end
+
+        it 'location' do
+            decorated_organization = ActiveDecorator::Decorator.instance.decorate(institution)
+        
+            expect(decorated_organization.institution_location).to eq('World+2')
+            expect(decorated_organization.institution_location).to include("World+")
+          end
+
+        it 'state' do
+        decorated_organization = ActiveDecorator::Decorator.instance.decorate(institution)
+    
+        expect(decorated_organization.current_state).to eq('not_verified')
+        end
+      end
 end
--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator do
+    let(:user) { build(:user) }
+
+    describe 'user' do
+        it 'full name' do
+          decorated_organization = ActiveDecorator::Decorator.instance.decorate(user)
+      
+          expect(decorated_organization.full_name).to eq('First+1 Last+1')
+          expect(decorated_organization.full_name).to include("First+")
+          expect(decorated_organization.full_name).to include("Last+")
+        end
+
+        it 'email address' do
+            decorated_organization = ActiveDecorator::Decorator.instance.decorate(user)
+        
+            expect(decorated_organization.email_address).to eq('email+2@example.com')
+            expect(decorated_organization.email_address).to include("email+")
+            expect(decorated_organization.email_address).to include("@example.com")
+          end
+
+          it 'state' do
+            decorated_organization = ActiveDecorator::Decorator.instance.decorate(user)
+        
+            expect(decorated_organization.current_state).to eq('not_verified')
+          end
+      end
 end
