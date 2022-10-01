0b8e5df940b288c93503a9d5e2c39def14a86a6e
Sam-2019 <samtmartey@gmail.com>
2022-10-01 20:20:13 +0100

    linted shared_func

--- app/utilities/shared_func.rb
+++ app/utilities/shared_func.rb
@@ -3,10 +3,10 @@ module SharedFunc
     def self.random_alpha_numeric(size = 14)
         charset = %w[2 3 4 6 7 9 A C D E F G H J K L M N P Q R T V W X Y Z]
         (0...size).map { charset.to_a[rand(charset.size)] }.join
-      end
+    end
 
-      def self.random_code(size = 8)
+    def self.random_code(size = 8)
         charset = %w[0 1 2 3 4 5 6 7 8 9]
         (0...size).map { charset.to_a[rand(charset.size)] }.join
-      end
+    end
 end
