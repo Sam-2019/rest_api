86293a94ed7db537e6de69784ee1e687fb4b7a88
Sam-2019 <samtmartey@gmail.com>
2022-10-02 16:39:46 +0100

    added blocker for no email & name

--- app/mailers/institution_mailer.rb
+++ app/mailers/institution_mailer.rb
@@ -1,13 +1,19 @@
 class InstitutionMailer < ApplicationMailer
     def welcome_email(institution)
-        mail(to: "#{institution.name} <#{institution.email}>", subject: 'Welcome to My Awesome Site')
+        return false if institution.email.blank? && institution.name.blank?
+        
+        mail(to: "#{institution.name} <#{institution.email}>", subject: 'Welcome to My Awesome Site') if institution.email.present?
     end
 
     def profile_update_email(institution)
-        mail(to: "#{institution.name} <#{institution.email}>", subject: 'Profile Update')
+        return false if institution.email.blank? && institution.name.blank?
+
+        mail(to: "#{institution.name} <#{institution.email}>", subject: 'Profile Update') if institution.email.present?
     end
 
     def account_destroy_email(institution)
-        mail(to: "#{institution.name} <#{institution.email}>", subject: 'Account Deletion')
+        return false if institution.email.blank? && institution.name.blank?
+
+        mail(to: "#{institution.name} <#{institution.email}>", subject: 'Account Deletion') if institution.email.present?
     end
 end
