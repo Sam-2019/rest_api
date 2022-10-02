26eb1ecc598470081e5f3bf270892d9d67703ee1
Sam-2019 <samtmartey@gmail.com>
2022-10-02 16:40:18 +0100

    added blocker for no email & name

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,31 @@
 class UserMailer < ApplicationMailer
+
+    @filepath = InfoPdf::PDF_DOWNLOAD_FOLDER
+
     def welcome_email(user)
-        @user = ActiveDecorator::Decorator.instance.decorate(user) 
-        mail(to: "#{@user.name} <#{@user.email}>", subject: 'Welcome to My Awesome Site')
+        @user = ActiveDecorator::Decorator.instance.decorate(user)
+        return false if @user.email.blank? && @user.name.blank? 
+
+        if FileTest.exist?("#{@filepath}#{@user.name}.pdf")
+            attachments["File.pdf"] = File.read("#{@filepath}#{@user.name}.pdf")
+            mail(to: "#{@user.name} <#{@user.email}>", subject: 'Welcome to My Awesome Site')
+        else
+            InfoPdf::User.new(@user.name)
+            UserMailer.welcome_email(@user.name).deliver_later(wait: 15.seconds)
+        end
     end
 
     def profile_update_email(user)
-        @user = ActiveDecorator::Decorator.instance.decorate(user) 
+        @user = ActiveDecorator::Decorator.instance.decorate(user)
+        return false if @user.email.blank? && @user.name.blank?
+
         mail(to: "#{@user.name} <#{@user.email}>", subject: 'Profile Update')
     end
 
     def account_destroy_email(user)
-        @user = ActiveDecorator::Decorator.instance.decorate(user) 
+        @user = ActiveDecorator::Decorator.instance.decorate(user)
+        return false if @user.email.blank? && @user.name.blank?
+
         mail(to: "#{@user.name} <#{@user.email}>", subject: 'Account Deletion')
     end
 end
