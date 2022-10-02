b8a9df9687319e2e222da3e0f8c9e7fe8c0c0a9e
Sam-2019 <samtmartey@gmail.com>
2022-10-02 07:48:23 +0100

    added test for state

--- spec/decorators/institution_decorator_spec.rb
+++ spec/decorators/institution_decorator_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe InstitutionDecorator do
-    let(:institution) { build(:institution) }
-    let(:decorated_institution) { ActiveDecorator::Decorator.instance.decorate(institution) }
+  let(:institution) { build(:institution) }
+  let(:verified_institution) { build(:institution, state: 'verified') }
+  let(:decorated_institution) { ActiveDecorator::Decorator.instance.decorate(institution) }
+  let(:decorated_verified_institution) { ActiveDecorator::Decorator.instance.decorate(verified_institution) }
 
-    describe 'institution' do
-        it 'name' do
-          expect(decorated_institution.institution_name).to eq('School+1')
-          expect(decorated_institution.institution_name).to include("School+")
-        end
+  describe 'institution' do
+    it 'name' do
+      expect(decorated_institution.institution_name).to eq('School+1')
+      expect(decorated_institution.institution_name).to include("School+")
+    end
 
-        it 'location' do
-            expect(decorated_institution.institution_location).to eq('World+2')
-            expect(decorated_institution.institution_location).to include("World+")
-          end
+    it 'location' do
+      expect(decorated_institution.institution_location).to eq('World+2')
+      expect(decorated_institution.institution_location).to include("World+")
+    end
 
-        it 'state' do
-        expect(decorated_institution.current_state).to eq('not_verified')
-        end
-      end
+    it 'state' do
+      expect(decorated_institution.current_state).to eq('not_verified')
+      expect(decorated_verified_institution.current_state).to eq('verified')
+    end
+  end
 end
--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator do
-    let(:user) { build(:user) }
-    let(:decorated_user) { ActiveDecorator::Decorator.instance.decorate(user) }
+  let(:user) { build(:user) }
+  let(:verified_user) { build(:user, state: 'verified') }
+  let(:decorated_user) { ActiveDecorator::Decorator.instance.decorate(user) }
+  let(:decorated_verified_user) { ActiveDecorator::Decorator.instance.decorate(verified_user) }
 
-    describe 'user' do
-        it 'full name' do      
-          expect(decorated_user.name).to eq('First+1 Last+1')
-          expect(decorated_user.name).to include("First+")
-          expect(decorated_user.name).to include("Last+")
-        end
+  describe 'user' do
+    it 'full name' do      
+      expect(decorated_user.name).to eq('First+1 Last+1')
+      expect(decorated_user.name).to include("First+")
+      expect(decorated_user.name).to include("Last+")
+    end
 
-        it 'email address' do
-            expect(decorated_user.email_address).to eq('email+2@example.com')
-            expect(decorated_user.email_address).to include("email+")
-            expect(decorated_user.email_address).to include("@example.com")
-          end
+    it 'email address' do
+      expect(decorated_user.email_address).to eq('email+2@example.com')
+      expect(decorated_user.email_address).to include("email+")
+      expect(decorated_user.email_address).to include("@example.com")
+    end
 
-          it 'state' do
-            expect(decorated_user.current_state).to eq('not_verified')
-          end
-      end
+    it 'state' do
+      expect(decorated_user.current_state).to eq('not_verified')
+      expect(decorated_verified_user.current_state).to eq('verified')
+    end
+  end
 end
