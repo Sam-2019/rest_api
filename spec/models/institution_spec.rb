047829f91bd9e0bfe860040c94043dadda81f2c5
Sam-2019 <samtmartey@gmail.com>
2022-10-03 23:29:02 +0100

    added instituion aasm

--- spec/models/institution_spec.rb
+++ spec/models/institution_spec.rb
@@ -23,4 +23,63 @@ RSpec.describe Institution, type: :model do
       expect(verified_institution.state).to eq("verified")
     end
   end
+
+  describe "Transitions" do
+    it "should have state not_verified" do
+      expect(institution).to have_state(:not_verified)
+      expect(institution).not_to have_state(:verified)
+      expect(institution).not_to have_state(:not_approved)
+      expect(institution).not_to have_state(:approved)
+    end
+
+    it "should transition from not_verified to verified" do
+      expect(institution).to have_state(:not_verified)
+      expect(institution).to transition_from(:not_verified).to(:verified).on_event(:verify)
+      expect(institution).to have_state(:verified)
+    end
+
+    it "should transition from verified to not_approved" do
+      institution = create(:institution, state: "verified")
+      expect(institution).to have_state(:verified)
+      expect(institution).to transition_from(:verified).to(:not_approved).on_event(:pending_approval)
+      expect(institution).to have_state(:not_approved)
+    end
+
+    it "should transition from not_approved to approved" do
+      institution = create(:institution, state: "not_approved")
+      expect(institution).to have_state(:not_approved)
+      expect(institution).to transition_from(:not_approved).to(:approved).on_event(:approve)
+      expect(institution).to have_state(:approved)
+    end
+
+    it "should not transition from not_verified" do
+      expect(institution).to have_state(:not_verified)
+      expect(institution).to_not allow_transition_to(:not_approved)
+      expect(institution).to_not allow_transition_to(:approved)
+    end
+
+    it "should not transition from verified" do
+      institution = create(:institution, state: "verified")
+      expect(institution).to have_state(:verified)
+      expect(institution).to_not allow_transition_to(:not_verified)
+      expect(institution).to_not allow_transition_to(:approved)
+    end
+
+    it "should not transition from not_approved" do
+      institution = create(:institution, state: "not_approved")
+      expect(institution).to have_state(:not_approved)
+      expect(institution).to_not allow_transition_to(:not_verified)
+      expect(institution).to_not allow_transition_to(:verified)
+    end
+
+    it "should not transition from approved" do
+      institution = create(:institution, state: "approved")
+      expect(institution).to have_state(:approved)
+      expect(institution).to_not allow_transition_to(:not_verified)
+      expect(institution).to_not allow_transition_to(:verified)
+      expect(institution).to_not allow_transition_to(:not_approved)
+      expect(institution).to_not allow_transition_to(:verified)
+    end
+
+  end
 end
