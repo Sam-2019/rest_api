2a2340af59a8fd98ae714838b2cef027247069cf
Sam-2019 <samtmartey@gmail.com>
2022-10-05 00:41:33 +0100

    added pdf content

--- app/services/info_pdf/institution.rb
+++ app/services/info_pdf/institution.rb
@@ -1,4 +1,5 @@
 require "prawn"
+require "securerandom"
 
 module InfoPdf
   class Institution
@@ -34,7 +35,44 @@ module InfoPdf
         :bold => RAILS_ROOT_PATH.join("app/assets/fonts/Montserrat/Montserrat-Bold.ttf"),
         :bold_italic => RAILS_ROOT_PATH.join("app/assets/fonts/Montserrat/Montserrat-BoldItalic.ttf"),
       })
-      @document.text "Hello #{@institution}"
+
+      timestamp = Time.current
+
+      @document.bounding_box [@document.bounds.left, @document.bounds.top], width: @document.bounds.width do
+        @document.stroke_color "ff0000"
+        @document.stroke_horizontal_rule
+        @document.move_down(5)
+        @document.font "Courier"
+        @document.text "#{timestamp.strftime("%A, %d %b %Y at %-I:M %p")} / #{SecureRandom.hex}", size: 7, character_spacing:1, color:"999999"
+      end
+      @document.move_down(5)
+      @document.font "Montserrat"
+      @document.text "Hello #{@institution}", align: :left
+      @document.canvas do
+        @document.fill_circle [@document.bounds.left, @document.bounds.top], 30
+        @document.fill_circle [@document.bounds.right, @document.bounds.top], 30
+        @document.fill_circle [@document.bounds.right, @document.bounds.bottom], 30
+        @document.fill_circle [0, 0], 30
+      end
+
+      @document.start_new_page
+      @document.bounding_box [@document.bounds.left, @document.bounds.top], width: @document.bounds.width do
+        @document.stroke_color "ff0000"
+        @document.stroke_horizontal_rule
+        @document.move_down(5)
+        @document.font "Courier"
+        @document.text "#{timestamp.strftime("%A, %d %b %Y at %-I:M %p")} / #{SecureRandom.hex}", size: 7, character_spacing:1, color:"999999"
+      end
+      @document.move_down(5)
+      @document.font "Courier"
+      @document.text "See. We've left the previous page behind.", align: :right
+      @document.canvas do
+        @document.fill_circle [@document.bounds.left, @document.bounds.top], 30
+        @document.fill_circle [@document.bounds.right, @document.bounds.top], 30
+        @document.fill_circle [@document.bounds.right, @document.bounds.bottom], 30
+        @document.fill_circle [0, 0], 30
+      end
+
       @document.render_file @filepath
     end
   end
