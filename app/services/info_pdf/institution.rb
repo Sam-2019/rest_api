fd96e76de191a2a1d1f8f1553920c38157da2bbd
Sam-2019 <samtmartey@gmail.com>
2022-10-06 23:20:01 +0100

    code cleanup

--- app/services/info_pdf/institution.rb
+++ app/services/info_pdf/institution.rb
@@ -15,9 +15,29 @@ module InfoPdf
         unless FileTest.exist?(@filepath)
           write_pdf
       end
-   end
-  
-    def write_pdf
+    end
+
+    def canvas
+        @document.canvas do
+        @document.fill_circle [@document.bounds.left, @document.bounds.top], 30
+        @document.fill_circle [@document.bounds.right, @document.bounds.top], 30
+        @document.fill_circle [@document.bounds.right, @document.bounds.bottom], 30
+        @document.fill_circle [0, 0], 30
+      end
+    end
+
+    def bounding
+      timestamp = Time.current
+      @document.bounding_box [@document.bounds.left, @document.bounds.top], width: @document.bounds.width do
+        @document.stroke_color "ff0000"
+        @document.stroke_horizontal_rule
+        @document.move_down(5)
+        @document.font "Courier"
+        @document.text "#{timestamp.strftime("%A, %d %b %Y at %-I:M %p")} / #{SecureRandom.hex}", size: 7, character_spacing:1, color:"999999"
+      end
+    end
+
+    def metadata
       info = {
         Title: "#{@institution}-name.pdf",
         Author: 'Rest API',
@@ -26,9 +46,11 @@ module InfoPdf
         Creator: 'Rest API App',
         Producer: 'Prawn',
         CreationDate: Time.now
-       }
-  
-      @document = Prawn::Document.new(page_size: "A5", page_layout: :landscape, info: info)
+        }
+    end
+
+    def write_pdf
+      @document = Prawn::Document.new(page_size: "A5", page_layout: :landscape, info: metadata)
       @document.font_families.update("Montserrat" => {
         :normal => RAILS_ROOT_PATH.join("app/assets/fonts/Montserrat/Montserrat-Regular.ttf"),
         :italic => RAILS_ROOT_PATH.join("app/assets/fonts/Montserrat/Montserrat-Italic.ttf"),
@@ -36,43 +58,24 @@ module InfoPdf
         :bold_italic => RAILS_ROOT_PATH.join("app/assets/fonts/Montserrat/Montserrat-BoldItalic.ttf"),
       })
 
-      def canvas
-          @document.canvas do
-          @document.fill_circle [@document.bounds.left, @document.bounds.top], 30
-          @document.fill_circle [@document.bounds.right, @document.bounds.top], 30
-          @document.fill_circle [@document.bounds.right, @document.bounds.bottom], 30
-          @document.fill_circle [0, 0], 30
-        end
-      end
-
-      timestamp = Time.current
-
-      @document.bounding_box [@document.bounds.left, @document.bounds.top], width: @document.bounds.width do
-        @document.stroke_color "ff0000"
-        @document.stroke_horizontal_rule
-        @document.move_down(5)
-        @document.font "Courier"
-        @document.text "#{timestamp.strftime("%A, %d %b %Y at %-I:M %p")} / #{SecureRandom.hex}", size: 7, character_spacing:1, color:"999999"
-      end
+      bounding
       @document.move_down(5)
       @document.font "Montserrat"
       @document.text "Hello #{@institution}", align: :left
       canvas
 
       @document.start_new_page
-      @document.bounding_box [@document.bounds.left, @document.bounds.top], width: @document.bounds.width do
-        @document.stroke_color "ff0000"
-        @document.stroke_horizontal_rule
-        @document.move_down(5)
-        @document.font "Courier"
-        @document.text "#{timestamp.strftime("%A, %d %b %Y at %-I:M %p")} / #{SecureRandom.hex}", size: 7, character_spacing:1, color:"999999"
-      end
+      bounding
       @document.move_down(5)
       @document.font "Courier"
       @document.text "See. We've left the previous page behind.", align: :right
+      @document.move_down(5)
+      @document.font "Courier"
+      @document.text "Yeah.", align: :right
       canvas
 
       @document.render_file @filepath
     end
   end
 end
+
