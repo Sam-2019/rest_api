d7c8b0f70e22128da994ca80be9fac60235f0af3
Sam-2019 <samtmartey@gmail.com>
2022-10-08 13:05:02 +0100

    addedn default value to soft_delete

--- db/migrate/20221008070807_add_soft_delete_column_to_users.rb
+++ db/migrate/20221008070807_add_soft_delete_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddSoftDeleteColumnToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :soft_delete, :boolean
+    add_column :users, :soft_delete, :boolean, default: 0
   end
 end
--- db/migrate/20221008070831_add_soft_delete_column_to_institutions.rb
+++ db/migrate/20221008070831_add_soft_delete_column_to_institutions.rb
@@ -1,5 +1,5 @@
 class AddSoftDeleteColumnToInstitutions < ActiveRecord::Migration[7.0]
   def change
-    add_column :institutions, :soft_delete, :boolean
+    add_column :institutions, :soft_delete, :boolean, default: 0
   end
 end
