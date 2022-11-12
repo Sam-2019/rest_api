05d45ff12c0715794d63574f549f9773f739757a
Sam-2019 <samtmartey@gmail.com>
2022-11-12 10:23:18 +0000

    updated welcome_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,9 +8,9 @@ class UserMailer < ApplicationMailer
 
         if FileTest.exist?("#{@filepath}#{@user.name}.pdf")
             attachments["File.pdf"] = File.read("#{@filepath}#{@user.name}.pdf")
-            mail(to: "#{@user.name} <#{@user.email_address}>", subject: 'Welcome to My Awesome Site')
+            build(user, "Welcome to My Awesome Site")
         else
-            InfoPdf::User.new(@user.name)
+            InfoPdf::User.new(@user)
             UserMailer.welcome_email(user).deliver_later(wait: 15.seconds)
         end
     end
