e7c96c5c994923f0bb4696ef338f0722c5dc3dec
Sam-2019 <samtmartey@gmail.com>
2022-11-19 10:49:26 +0000

    added test for institution pdf

---
+++ spec/pdfs/institution_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Reports do
+  let(:institution) { build(:institution) }
+  let(:decorated_institution) { ActiveDecorator::Decorator.instance.decorate(institution) }
+
+  let(:file) { "#{RAILS_ROOT_PATH}/pdf_downloads/#{decorated_institution.name}.pdf" }
+  let(:pdf) { Reports::Pdf::Institution.new(institution).write_pdf }
+  let(:pdf_content) { PDF::Reader.new(file).page(1).to_s }
+
+  it 'contains institution name' do
+    expect(pdf).to be_truthy
+    expect(pdf_content).to include("#{decorated_institution.name}")
+  end
+end
