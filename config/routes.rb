d99dc8fe546f7b53ffc8e0b270a42e8cedcf1152
Sam-2019 <samtmartey@gmail.com>
2022-12-22 07:36:21 +0000

    motor admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
+  mount Motor::Admin => '/motor_admin'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
