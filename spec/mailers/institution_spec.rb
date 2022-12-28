c5f0bfe1acd8c88f008a38a0f47b9cd2a96e78ed
Sam-2019 <samtmartey@gmail.com>
2022-12-28 20:15:41 +0000

    added institution mailer spec

--- spec/mailers/institution_spec.rb
+++ spec/mailers/institution_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe InstitutionMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:institution) { build(:institution) }
+  let(:institution_without_name) { build(:institution, name: nil) }
+  let(:institution_without_email) { build(:institution, email: nil) }
+
+  describe "Validation" do
+    it "without name" do
+      email = InstitutionMailer.welcome_email(institution_without_name).deliver_now
+      expect(email).to be_falsy
+    end
+
+    it "without email" do
+      email = InstitutionMailer.welcome_email(institution_without_email).deliver_now
+      expect(email).to be_falsy
+    end
+
+    it "with name and email" do
+      email = InstitutionMailer.welcome_email(@data).deliver_now
+      expect(email).to be_truthy
+    end
+  end
 end
