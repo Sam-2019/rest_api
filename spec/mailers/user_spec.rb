d5bf116ab3f4e46a260d3fd434b36645966ae751
Sam-2019 <samtmartey@gmail.com>
2022-12-28 20:15:28 +0000

    added user mailer spec

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+  let(:user_without_name) { build(:user, name: nil) }
+  let(:user_without_email) { build(:user, email: nil) }
+
+  describe "Validation" do
+    it "without name" do
+      email = UserMailer.welcome_email(user_without_name).deliver_now
+      expect(email).to be_falsy
+    end
+
+    it "without email" do
+      email = UserMailer.welcome_email(user_without_email).deliver_now
+      expect(email).to be_falsy
+    end
+  
+    it "with name and email" do
+      email = UserMailer.welcome_email(user).deliver_now
+      expect(pdf).to be_truthy
+    end
+  end
 end
