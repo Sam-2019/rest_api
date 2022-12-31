d546e05a7287cb2e539fe8e588dafd5e10bb1f61
Sam-2019 <samtmartey@gmail.com>
2022-12-31 16:25:16 +0000

    mailer preview update

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,17 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user
 class UserMailerPreview < ActionMailer::Preview
     def welcome_email
-        UserMailer.with(user: User.first).welcome_email
+        user = User.last
+        UserMailer.welcome_email(user)
     end
       
-    def profile_update_email(user)
-        UserMailer.with(user: User.first).profile_update_email
+    def profile_update_email
+        user = User.last
+        UserMailer.profile_update_email(user)
     end
 
-    def account_destroy_email(user)
-        UserMailer.with(user: User.first).account_destroy_email
+    def account_destroy_email
+        user = User.last
+        UserMailer.account_destroy_email(user)
     end
 end
