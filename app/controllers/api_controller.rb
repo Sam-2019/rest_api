9f6b56fbf2dd2b1435f1491501c919f367ca0fa9
Sam-2019 <samtmartey@gmail.com>
2023-03-18 19:19:52 +0000

    added exception handler

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class ApiController < ActionController::API
+  rescue_from (ActiveRecord::RecordNotFound) { |exception| exception_error }
   include ActionController::MimeResponds
+
+  private
+
+  def exception_error
+    render json: true, status: :no_content
+  end
 end
