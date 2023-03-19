ad4c1f08fc456fde595e3d5fd35efe1e8483ff6c
Sam-2019 <samtmartey@gmail.com>
2023-03-19 15:27:10 +0000

    routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@
 
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  # mount Motor::Admin => "/motor_admin"
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
-
+  
   namespace :api do
     namespace :v1 do
       resources :user
@@ -18,22 +16,6 @@ Rails.application.routes.draw do
       get "dwnld/pdf/institution/:id" => "institution#pdf", :as => :download_spreadsheet_institution
     end
   end
-
-  # namespace :admin do
-  #   resources :name, only: %i(index show new create edit update destroy)
-  # end
-
-  # get 'institution/index'
-  # get 'institution/show'
-  # patch 'institution/update'
-  # delete 'institution/destroy'
-  # post 'institution/create'
-  # get 'user/index'
-  # get 'user/show'
-  # patch 'user/update'
-  # delete 'user/destroy'
-  # post 'user/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
